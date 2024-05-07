b6f71f4255d568a50b64d1304cd4255c0a729d8d
NaveenReddyMiniPuri123 <167668786+NaveenReddyMiniPuri123@users.noreply.github.com>
2024-05-07 23:06:05 +0530

    Update iv_gt_tb.v

--- iv_gt_tb.v
+++ iv_gt_tb.v
@@ -3,7 +3,7 @@ module iv_gt_tb;
 reg clk,RN;
 wire [31:0]WB_OUT,NPC;
 
-ins_RV32EC RV32(clk,RN,NPC,WB_OUT);
+  iv_gt(gt(clk,RN,NPC,WB_OUT);
 
 
 always #3 clk=!clk;
@@ -12,8 +12,8 @@ initial begin
 RN  = 1'b1;
 clk = 1'b1;
 
-$dumpfile ("ins_RV32EC.vcd"); //by default vcd
-$dumpvars (0, ins_RV32EC_tb);
+  $dumpfile ("iv_gt.vcd"); //by default vcd
+  $dumpvars (0, iv_gt_tb);
   
   #5 RN = 1'b0;
   
