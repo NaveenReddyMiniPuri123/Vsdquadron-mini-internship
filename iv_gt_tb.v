298ab16a04049b680dc089d4e68d065b1e9c5cbd
NaveenReddyMiniPuri123 <167668786+NaveenReddyMiniPuri123@users.noreply.github.com>
2024-05-07 23:10:22 +0530

    Update iv_gt_tb.v

--- iv_gt_tb.v
+++ iv_gt_tb.v
@@ -3,8 +3,7 @@ module iv_gt_tb;
 reg clk,RN;
 wire [31:0]WB_OUT,NPC;
   
-  iv_gt gt(clk,RN,NPC,WB_OUT);
-
+iv_gt gt(clk,RN,NPC,WB_OUT);
 
 always #3 clk=!clk;
 
@@ -12,12 +11,12 @@ initial begin
 RN  = 1'b1;
 clk = 1'b1;
 
-  $dumpfile ("iv_gt.vcd"); //by default vcd
-  $dumpvars (0, iv_gt_tb);
+$dumpfile ("iv_gt.vcd"); //by default vcd
+$dumpvars (0, iv_gt_tb);
   
-  #5 RN = 1'b0;
+#5 RN = 1'b0;
   
-  #300 $finish;
+#300 $finish;
 
 end
 endmodule
